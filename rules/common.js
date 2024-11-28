37e5969f56a03e9a0d425699c1670c62c87a9d83
vchikalkin <djchikalkin@gmail.com>
2024-11-28 11:39:02 +0300

    rule canonical/filename-match-regex:  allow -

--- rules/common.js
+++ rules/common.js
@@ -3,6 +3,13 @@ module.exports = {
   name: 'Common rules override',
   rules: {
     'canonical/filename-match-exported': 'off',
+    'canonical/filename-match-regex': [
+      1,
+      {
+        ignoreExporting: false,
+        regex: '^[A-Za-z]+(?:[A-Za-z0-9-]*\\.[A-Za-z0-9-]+)*\\d*$',
+      },
+    ],
     'func-style': [
       'warn',
       'declaration',
