017166ab66a583f8d214009fbb422b8652bd1256
vchikalkin <djchikalkin@gmail.com>
2025-01-11 12:56:35 +0300

    feat: add typescript config

--- configs/typescript.js
+++ configs/typescript.js
@@ -1,32 +1,19 @@
 const auto = require('eslint-config-canonical/configurations/auto');
 const sonarjs = require('eslint-plugin-sonarjs');
 const rules = require('../rules');
-const pluginNext = require('@next/eslint-plugin-next');
 
 /** @type {import('eslint').Linter.Config} */
 module.exports = [
-  { name: 'ESLint Config Awesome - React/Next Typescript' },
+  { name: 'ESLint Config Awesome - Typescript' },
   ...auto,
   sonarjs.configs.recommended,
   {
     plugins: {
       import: require('eslint-plugin-import'),
-      react: require('eslint-plugin-react'),
-    },
-  },
-  {
-    plugins: {
-      '@next/next': pluginNext,
-    },
-    rules: {
-      ...pluginNext.configs.recommended.rules,
-      ...pluginNext.configs['core-web-vitals'].rules,
     },
   },
   rules.common,
-  rules.react,
   rules.sonar,
-  rules.next,
   {
     ignores: [
       '**/node_modules',
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 const reactTypescript = require('./configs/react-typescript');
+const typescript = require('./configs/typescript');
 
-module.exports = { 'react-typescript': reactTypescript };
+module.exports = { 'react-typescript': reactTypescript, typescript };
